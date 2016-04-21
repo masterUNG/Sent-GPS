9cb77a1dff9dc3785c890bdad56b5ef1fc1ba802
masterUNG <phrombutr@gmail.com>
2016-04-21 11:57:38 +0700

    Intent to Main finish

--- app/src/main/java/appewtc/masterung/sentgps/FirstActivity.java
+++ app/src/main/java/appewtc/masterung/sentgps/FirstActivity.java
@@ -1,7 +1,9 @@
 package appewtc.masterung.sentgps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }   // Main Method
+
+    public void clickAddPlate(View view) {
+        startActivity(new Intent(FirstActivity.this, MainActivity.class));
     }
-}
+
+}   // Main Class
