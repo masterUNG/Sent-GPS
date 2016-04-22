8ce352da5fa04f98b0cba4848b331aa48cbbb001
masterUNG <phrombutr@gmail.com>
2016-04-22 15:09:44 +0700

    Noti OK

--- app/src/main/java/appewtc/masterung/sentgps/FirstActivity.java
+++ app/src/main/java/appewtc/masterung/sentgps/FirstActivity.java
@@ -1,10 +1,15 @@
 package appewtc.masterung.sentgps;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -121,6 +126,22 @@ public class FirstActivity extends AppCompatActivity {
     private void myNotification() {
 
         Toast.makeText(this, "เข้าในพื้นที่แล้ว", Toast.LENGTH_SHORT).show();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.build4);
+        builder.setTicker("Driving Better");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setContentTitle("เข้าในพื้นที่แล้ว");
+        builder.setContentText("เข้าในพื้นที่แล้ว");
+        builder.setAutoCancel(false);
+
+
+        Uri soundUri = RingtoneManager.getDefaultUri(Notification.DEFAULT_SOUND);
+        builder.setSound(soundUri);
+
+        android.app.Notification notification = builder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(1000, notification);
 
     }   // myNotification
 
